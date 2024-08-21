12ed5f4cfaec9d0c6f20f796672619dcc4618a12
ibraheemishfaq171 <48013617+ibraheemishfaq171@users.noreply.github.com>
2024-08-21 23:28:30 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -29,11 +29,7 @@ const app = express();
 const _dirname = path.resolve();
 app.use(express.json());
 app.use(cors());
-app.use(function (req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-})
+
 app.use(cookieParser());
 app.listen(3000, () => {
   console.log("server is running on 3000 port");
